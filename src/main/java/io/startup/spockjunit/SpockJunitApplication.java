152355a2e728cc1153129a0af5e8d1c8d0769b1a
home <gnosia93@naver.com>
2018-12-30 23:32:21 +0900

    inital.

---
+++ src/main/java/io/startup/spockjunit/SpockJunitApplication.java
@@ -0,0 +1,14 @@
+package io.startup.spockjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockJunitApplication.class, args);
+	}
+
+}
+
